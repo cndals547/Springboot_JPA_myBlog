1631fb6447a93afb5b753a7a5c75e11f609ff23d
Chungmin_Lee <46112744+cndals547@users.noreply.github.com>
2021-03-12 15:31:34 +0900

    글목록 보기

--- src/main/java/com/chungmin/blog/controller/BoardController.java
+++ src/main/java/com/chungmin/blog/controller/BoardController.java
@@ -1,14 +1,21 @@
 package com.chungmin.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.chungmin.blog.service.BoardService;
 
 @Controller
 public class BoardController {
 	
+	@Autowired
+	private BoardService boardSerivce;
+	
 	@GetMapping({"", "/"})
-	public String index() {
-		// /WEB-INF/views/index.jsp
+	public String index(Model model) {
+		model.addAttribute("boards", boardSerivce.글목록());
 		return "index";
 	}
 	
